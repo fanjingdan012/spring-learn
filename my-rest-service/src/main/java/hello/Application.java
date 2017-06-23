ccaac559d78f5b86f5e641a9fe1deff0b6cb497d
Judy Fan <judy.fan@sap.com>
2017-06-23 09:00:57 +0800

    spring boot init

---
+++ my-rest-service/src/main/java/hello/Application.java
@@ -0,0 +1,17 @@
+package hello;
+
+/**
+ * Created by I312177 on 6/23/2017.
+ */
+
+
+        import org.springframework.boot.SpringApplication;
+        import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Application.class, args);
+    }
+}
